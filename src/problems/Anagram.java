14688898502e6ff5c8de4b47ce5de30ab3660e42
guancao <guancao@gmail.com>
2018-04-08 11:23:19 -0400

    worked on problems

--- src/problems/Anagram.java
+++ src/problems/Anagram.java
@@ -1,5 +1,7 @@
 package problems;
 
+import java.util.ArrayList;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -8,5 +10,43 @@ public class Anagram {
     public static void main(String[] args) {
         //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
         //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+        String str1 = "ACD";
+        String str2 = "TCA";
+        if(str1.length()!= str2.length()) System.out.println("This is two strings are NOT anagram!");
+
+        ArrayList<Character> charList1 = new ArrayList<>();
+        ArrayList<Character> charList2 = new ArrayList<>();
+
+        if(str1.length()==str2.length()) try {
+            for (int i = 0; i < str1.length(); i++) {
+                charList1.add(str1.charAt(i));
+                charList2.add(str2.charAt(i));
+            }
+            //sort characters in each list
+            sortList(charList1);
+            sortList(charList2);
+
+            //compare each character in the list
+            boolean isDiff = false;
+            for (int k=0; k<charList1.size(); k++){
+                if(charList1.get(k)!= charList2.get(k)) isDiff = true;
+            }
+            if(isDiff) System.out.println("This is two strings are NOT anagram!");
+            else System.out.println("This is two strings are anagram!!!");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void sortList(ArrayList<Character> list){
+        for (int i=1; i<list.size(); i++){
+            for(int j=i; j>0; j--){
+                if(list.get(j)<list.get(j-1)){
+                    Character temp = list.get(j);
+                    list.set(j, list.get(j-1));
+                    list.set(j-1, temp);
+                }
+            }
+        }
     }
 }
--- src/problems/DuplicateWord.java
+++ src/problems/DuplicateWord.java
@@ -1,5 +1,10 @@
 package problems;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -12,6 +17,42 @@ public class DuplicateWord {
          */
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        List<String> wordList = new ArrayList<String>();
+        Map<String, Integer> wordMap = new HashMap<>();
+        String[] words = st.split("\\W+");
+        //       for (String x: words) System.out.println(x);
+
+        for (int i = 0; i < words.length; i++) {
+            int count = 1;
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].equalsIgnoreCase(words[j])) {
+                    count++;
+                    words[j] = "";
+                }
+            }
+            wordMap.put(words[i], count);
+        }
+        //check the map
+//        for (Map.Entry entry : wordMap.entrySet()) System.out.println(entry.getKey() + " " + entry.getValue());
+
+        //find the duplicates
+        for (Map.Entry entry : wordMap.entrySet()) {
+            if ((int) entry.getValue() > 1)
+                System.out.println(entry.getKey() + " has duplicates, total appearance ::: " + entry.getValue());
+            ;
+        }
+
+        //find the average length of words
+        int lenSum = 0;
+        int counter = 0;
+        int avg = 0;
+        for (Map.Entry entry : wordMap.entrySet()) {
+            lenSum += entry.getKey().toString().length();
+ //           System.out.println(lenSum);
+            counter++;
+        }
+        avg = lenSum / counter;
+        System.out.println("The average length of words is :::" + avg);
 
     }
 
